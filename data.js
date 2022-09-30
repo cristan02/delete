7ae20b04f217cd321b5f4652d89e6af5572e82d1
Ashbourn D'Cunha <94105472+cristan02@users.noreply.github.com>
2022-09-30 14:14:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id : 1,
+        name : 'Ashbourn Dcunha',
+        age : 10,
+        image : 'avatar.png',
+    },
+    {
+        id : 2,
+        name : 'Moses Crasto',
+        age : 20,
+        image : 'avatar2.png',
+    },
+    {
+        id : 3,
+        name : 'Reeve Barreto',
+        age : 15,
+        image : 'otaku.png',
+    },
+    {
+        id : 4,
+        name : 'Elton John',
+        age : 20,
+        image : 'sailor.png',
+    }
+]
